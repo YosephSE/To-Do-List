94a81801e42f279779280ce3e60acdc5f607dedd
YosephSE <88291669+YosephSE@users.noreply.github.com>
2024-07-17 10:03:54 +0300

    initialized using vite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./build/**/*.{html,js}"],
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
