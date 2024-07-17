7724ad639209b4e33f57a6f8708faf4d1f73d121
YosephSE <88291669+YosephSE@users.noreply.github.com>
2024-07-17 11:43:53 +0300

    Header section and input box added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import { useState } from 'react'
+import taskIcon from './assets/task.png'
+import add from './assets/add.png'
 
 function App() {
-
+  const [toDos, setToDos] = useState([])
   return (
-    <>
-      Hello
-    </>
+   <div className="bg-sky-200 min-h-screen">
+   <div className="header h-16 flex justify-center items-center">
+   <img src={taskIcon} alt="Task Icon" className='h-full'/>
+   <h1 className='font-bold italic text-3xl'>Tasks</h1>
+   <div className=''><img src={add} alt="Add Icon" /></div>
+   </div>
+   <div className="input">
+    <input type="text" name="" id="inputbox" />
+   </div>
+   <div className="todos"></div>
+   </div>
   )
 }
 
